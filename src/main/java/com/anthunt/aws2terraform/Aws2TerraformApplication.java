f043480204fc94510712252dccc2fb91b0495dad
jeongyeol <cinnamond3@gmail.com>
2020-09-02 21:57:42 +0900

    initialize spring boot project with gradle #4

---
+++ src/main/java/com/anthunt/aws2terraform/Aws2TerraformApplication.java
@@ -0,0 +1,13 @@
+package com.anthunt.aws2terraform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aws2TerraformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aws2TerraformApplication.class, args);
+	}
+
+}
