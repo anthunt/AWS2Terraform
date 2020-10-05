319e862e184e94dc29d390c7e5e587d70f1d03ce
anthunt <anthunt01@gmail.com>
2020-10-05 17:21:20 +0900

    remove unused imports

--- terraform-generator-aws/src/main/java/com/anthunt/terraform/generator/aws/command/Ec2Commands.java
+++ terraform-generator-aws/src/main/java/com/anthunt/terraform/generator/aws/command/Ec2Commands.java
@@ -1,6 +1,5 @@
 package com.anthunt.terraform.generator.aws.command;
 
-import com.anthunt.terraform.generator.aws.client.AmazonClients;
 import com.anthunt.terraform.generator.aws.service.ec2.ExportInstances;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,6 @@ import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 import software.amazon.awssdk.services.ec2.Ec2Client;
-import software.amazon.awssdk.services.ec2.model.DescribeInstancesResponse;
-import software.amazon.awssdk.services.ec2.model.DescribeVpcsResponse;
 
 import javax.validation.Valid;
 
--- terraform-generator-aws/src/test/java/com/anthunt/terraform/generator/aws/TerraformTest.java
+++ terraform-generator-aws/src/test/java/com/anthunt/terraform/generator/aws/TerraformTest.java
@@ -1,25 +1,12 @@
 package com.anthunt.terraform.generator.aws;
 
 import com.anthunt.terraform.generator.aws.client.AmazonClients;
-import com.anthunt.terraform.generator.core.model.terraform.Terraform;
-import com.anthunt.terraform.generator.core.model.terraform.elements.*;
-import com.anthunt.terraform.generator.core.model.terraform.nodes.Maps;
-import com.anthunt.terraform.generator.core.model.terraform.nodes.Provider;
-import com.anthunt.terraform.generator.core.model.terraform.nodes.Resource;
-import com.anthunt.terraform.generator.core.model.terraform.types.ProviderType;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import software.amazon.awssdk.services.ec2.Ec2Client;
-import software.amazon.awssdk.services.ec2.model.*;
-
-import java.util.Base64;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @ActiveProfiles("custom")
