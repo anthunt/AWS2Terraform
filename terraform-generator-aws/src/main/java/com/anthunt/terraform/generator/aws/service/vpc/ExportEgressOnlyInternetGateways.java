df2baa941f921b47ea60eb00de7f9b724d41a0ab
anthunt <anthunt01@gmail.com>
2020-11-13 14:19:15 +0900

    add egressOnlyInternetGateway, routetable, subnet

--- terraform-generator-aws/src/main/java/com/anthunt/terraform/generator/aws/service/vpc/ExportEgressOnlyInternetGateways.java
+++ terraform-generator-aws/src/main/java/com/anthunt/terraform/generator/aws/service/vpc/ExportEgressOnlyInternetGateways.java
@@ -11,9 +11,9 @@ import com.anthunt.terraform.generator.core.model.terraform.nodes.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.ec2.Ec2Client;
-import software.amazon.awssdk.services.ec2.model.DescribeNatGatewaysResponse;
-import software.amazon.awssdk.services.ec2.model.NatGateway;
-import software.amazon.awssdk.services.ec2.model.NatGatewayAddress;
+import software.amazon.awssdk.services.ec2.model.DescribeEgressOnlyInternetGatewaysResponse;
+import software.amazon.awssdk.services.ec2.model.EgressOnlyInternetGateway;
+import software.amazon.awssdk.services.ec2.model.InternetGatewayAttachment;
 import software.amazon.awssdk.services.ec2.model.Tag;
 
 import java.util.List;
@@ -21,29 +21,31 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Service
-public class ExportNatGateways extends AbstractExport<Ec2Client> {
+public class ExportEgressOnlyInternetGateways extends AbstractExport<Ec2Client> {
+
     @Override
     protected Maps<Resource> export(Ec2Client client, CommonArgs commonArgs, ExtraArgs extraArgs) {
 
         Maps.MapsBuilder<Resource> resourceMapsBuilder = Maps.builder();
 
-        DescribeNatGatewaysResponse describeNatGatewaysResponse = client.describeNatGateways();
-        List<NatGateway> natGateways = describeNatGatewaysResponse.natGateways ();
+        DescribeEgressOnlyInternetGatewaysResponse describeEgressOnlyInternetGatewaysResponse = client.describeEgressOnlyInternetGateways();
+        List<EgressOnlyInternetGateway> egressOnlyInternetGateways = describeEgressOnlyInternetGatewaysResponse.egressOnlyInternetGateways();
 
         int i = 0;
-        for(NatGateway natGateway : natGateways) {
-            List<NatGatewayAddress> natGatewayAddresses = natGateway.natGatewayAddresses();
-            for(NatGatewayAddress natGatewayAddress : natGatewayAddresses) {
+        for(EgressOnlyInternetGateway egressOnlyInternetGateway : egressOnlyInternetGateways) {
+
+            List<InternetGatewayAttachment> internetGatewayAttachments = egressOnlyInternetGateway.attachments();
+
+            for(InternetGatewayAttachment internetGatewayAttachment : internetGatewayAttachments) {
                 resourceMapsBuilder.map(
                         Resource.builder()
-                                .api("aws_nat_gateway")
-                                .name("nat_gateway" + i)
+                                .api("aws_egress_only_internet_gateway")
+                                .name("egress_only_internet_gateway" + i)
                                 .arguments(
                                         TFArguments.builder()
-                                                .argument("allocation_id", TFString.build(natGatewayAddress.allocationId()))
-                                                .argument("subnet_id", TFString.build(natGateway.subnetId()))
+                                                .argument("vpc_id", TFString.build(internetGatewayAttachment.vpcId()))
                                                 .argument("tags", TFMap.build(
-                                                        natGateway.tags().stream()
+                                                        egressOnlyInternetGateway.tags().stream()
                                                                 .collect(Collectors.toMap(Tag::key, tag -> TFString.build(tag.value())))
                                                 ))
                                                 .build()
