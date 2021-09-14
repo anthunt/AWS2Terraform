70d63559b1fb9fba22e06dcbab04ea27dc55d60a
최정열 <cinnamond3@lgcns.com>
2021-09-14 18:16:57 +0900

    feature/export-subnets

--- terraform-generator-aws/src/test/resources/testData/vpc/expected/Subnet.tf
+++ terraform-generator-aws/src/test/resources/testData/vpc/expected/Subnet.tf
@@ -1,4 +1,4 @@
-resource aws_subnet subnet0 {
+resource aws_subnet subnet-01020304 {
 	availability_zone_id = "apne2-az2"
 	cidr_block = "172.31.16.0/20"
 	map_public_ip_on_launch = true
@@ -8,7 +8,7 @@ resource aws_subnet subnet0 {
 	}
 }
 
-resource aws_subnet subnet1 {
+resource aws_subnet subnet-02020304 {
 	availability_zone_id = "apne2-az1"
 	cidr_block = "172.31.0.0/20"
 	map_public_ip_on_launch = true
@@ -18,7 +18,7 @@ resource aws_subnet subnet1 {
 	}
 }
 
-resource aws_subnet subnet2 {
+resource aws_subnet subnet-03020304 {
 	availability_zone_id = "apne2-az3"
 	cidr_block = "172.31.32.0/20"
 	map_public_ip_on_launch = true
