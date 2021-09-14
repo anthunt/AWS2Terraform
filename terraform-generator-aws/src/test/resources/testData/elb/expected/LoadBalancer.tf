f70b1465088c7d6e7e8c6e35984768d1648e94fa
jeongyeol <cinnamond3@gmail.com>
2021-09-15 02:08:40 +0900

    feature/export-load-balancers

--- terraform-generator-aws/src/test/resources/testData/elb/expected/LoadBalancer.tf
+++ terraform-generator-aws/src/test/resources/testData/elb/expected/LoadBalancer.tf
@@ -15,14 +15,60 @@ resource aws_lb a000567db2d1f4d02b7493427dc88888 {
 	}
 }
 
-resource aws_lb a000567db2d1f4d02b7493427dc88888 {
-	name = "a000567db2d1f4d02b7493427dc88888"
+resource aws_lb b000567db2d1f4d02b7493427dc88888 {
+	name = "b000567db2d1f4d02b7493427dc88888"
 	internal = false
 	load_balancer_type = "application"
 	subnets = [
 		aws_subnet.subnet-05837fce269e60003.id,
 		aws_subnet.subnet-05837fce269e60004.id
 	]
+	ip_address_type = "ipv4"
+	enable_deletion_protection = false
+	enable_cross_zone_load_balancing = false
+	tags = {
+	}
+}
+
+resource aws_lb c000567db2d1f4d02b7493427dc88888 {
+	name = "c000567db2d1f4d02b7493427dc88888"
+	internal = false
+	load_balancer_type = "application"
+
+	subnet_mapping {
+		subnet_id = aws_subnet.subnet-05837fce269e60003.id
+		allocation_id  = "allication-01"
+	}
+
+
+	subnet_mapping {
+		subnet_id = aws_subnet.subnet-05837fce269e60004.id
+		allocation_id  = "allication-02"
+	}
+
+	ip_address_type = "ipv4"
+	enable_deletion_protection = false
+	enable_cross_zone_load_balancing = false
+	tags = {
+	}
+}
+
+resource aws_lb d000567db2d1f4d02b7493427dc88888 {
+	name = "d000567db2d1f4d02b7493427dc88888"
+	internal = false
+	load_balancer_type = "application"
+
+	subnet_mapping {
+		subnet_id = aws_subnet.subnet-05837fce269e60003.id
+		private_ipv4_address  = "10.0.1.15"
+	}
+
+
+	subnet_mapping {
+		subnet_id = aws_subnet.subnet-05837fce269e60004.id
+		private_ipv4_address  = "10.0.2.15"
+	}
+
 	ip_address_type = "ipv4"
 	enable_deletion_protection = false
 	enable_cross_zone_load_balancing = false
