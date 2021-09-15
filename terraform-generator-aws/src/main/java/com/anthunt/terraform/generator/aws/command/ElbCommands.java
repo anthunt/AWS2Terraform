5119a473b4690b8f32de4150da6c0c50d9d4ba07
최정열 <cinnamond3@lgcns.com>
2021-09-15 15:45:06 +0900

    feature/export-load-balancer-target-groups

--- terraform-generator-aws/src/main/java/com/anthunt/terraform/generator/aws/command/ElbCommands.java
+++ terraform-generator-aws/src/main/java/com/anthunt/terraform/generator/aws/command/ElbCommands.java
@@ -1,5 +1,6 @@
 package com.anthunt.terraform.generator.aws.command;
 
+import com.anthunt.terraform.generator.aws.service.elb.ExportLoadBalancerTargetGroups;
 import com.anthunt.terraform.generator.aws.service.elb.ExportLoadBalancers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.shell.standard.ShellComponent;
@@ -15,13 +16,21 @@ public class ElbCommands extends AbstractCommands {
 
     private ExportLoadBalancers exportLoadBalancers;
 
-    public ElbCommands(ExportLoadBalancers exportLoadBalancers) {
+    private ExportLoadBalancerTargetGroups exportLoadBalancerTargetGroups;
+
+    public ElbCommands(ExportLoadBalancers exportLoadBalancers, ExportLoadBalancerTargetGroups exportLoadBalancerTargetGroups) {
         this.exportLoadBalancers = exportLoadBalancers;
+        this.exportLoadBalancerTargetGroups = exportLoadBalancerTargetGroups;
     }
 
     @ShellMethod("Export terraform resources of LoadBalancers.")
     public void loadBalancers(@ShellOption(optOut = true) @Valid CommonArgs commonArgs) {
         exportLoadBalancers.exportTerraform(ElasticLoadBalancingV2Client.class, commonArgs);
+    }
+
+    @ShellMethod("Export terraform resources of LoadBalancerTargetGroups.")
+    public void loadBalancerTargetGroups(@ShellOption(optOut = true) @Valid CommonArgs commonArgs) {
+        exportLoadBalancerTargetGroups.exportTerraform(ElasticLoadBalancingV2Client.class, commonArgs);
     }
 
 }
