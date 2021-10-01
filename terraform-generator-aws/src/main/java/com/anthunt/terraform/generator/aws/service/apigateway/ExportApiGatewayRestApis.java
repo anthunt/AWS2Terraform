b8593682365baeb245f1376b9186605c588b875e
최정열 <cinnamond3@lgcns.com>
2021-10-01 15:06:57 +0900

    export-apigateway-rest-apis

---
+++ terraform-generator-aws/src/main/java/com/anthunt/terraform/generator/aws/service/apigateway/ExportApiGatewayRestApis.java
@@ -0,0 +1,128 @@
+package com.anthunt.terraform.generator.aws.service.apigateway;
+
+import com.anthunt.terraform.generator.aws.command.CommonArgs;
+import com.anthunt.terraform.generator.aws.command.ExtraArgs;
+import com.anthunt.terraform.generator.aws.service.AbstractExport;
+import com.anthunt.terraform.generator.aws.service.apigateway.model.AWSRestApi;
+import com.anthunt.terraform.generator.aws.service.apigateway.model.AWSStage;
+import com.anthunt.terraform.generator.core.model.terraform.elements.*;
+import com.anthunt.terraform.generator.core.model.terraform.nodes.Maps;
+import com.anthunt.terraform.generator.core.model.terraform.nodes.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.apigateway.ApiGatewayClient;
+import software.amazon.awssdk.services.apigateway.model.*;
+
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class ExportApiGatewayRestApis extends AbstractExport<ApiGatewayClient> {
+
+    @Override
+    protected Maps<Resource> export(ApiGatewayClient client, CommonArgs commonArgs, ExtraArgs extraArgs) {
+
+        List<AWSRestApi> awsRestApis = getAWSRestApis(client);
+
+        return getResourceMaps(awsRestApis);
+
+    }
+
+    List<AWSRestApi> getAWSRestApis(ApiGatewayClient client) {
+        GetRestApisResponse restApisResponse = client.getRestApis();
+        return restApisResponse.items().stream()
+                .peek(restApi -> log.debug("restApi => {}", restApi))
+                .map(restApi -> AWSRestApi.builder()
+                        .restApi(restApi)
+                        .awsStages(client.getStages(GetStagesRequest.builder()
+                                        .restApiId(restApi.id())
+                                        .build())
+                                .item().stream()
+                                .map(stage -> AWSStage.builder()
+                                        .stage(stage)
+                                        .deployment(client.getDeployment(GetDeploymentRequest.builder()
+                                                .restApiId(restApi.id())
+                                                .deploymentId(stage.deploymentId())
+                                                .build()))
+                                        .build())
+                                .collect(Collectors.toList()))
+
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    Maps<Resource> getResourceMaps(List<AWSRestApi> awsRestApis) {
+        Maps.MapsBuilder<Resource> resourceMapsBuilder = Maps.builder();
+        for (AWSRestApi awsRestApi : awsRestApis) {
+            RestApi restApi = awsRestApi.getRestApi();
+
+            resourceMapsBuilder.map(
+                            Resource.builder()
+                                    .api("aws_api_gateway_rest_api")
+                                    .name(restApi.name())
+                                    .argument("name", TFString.build(restApi.name()))
+                                    .argument("description", TFString.build(restApi.description()))
+                                    .argument("api_key_source", TFString.build(restApi.apiKeySourceAsString()))
+                                    .argument("disable_execute_api_endpoint", TFBool.build(restApi.disableExecuteApiEndpoint()))
+                                    .argument("endpoint_configuration", TFBlock.builder()
+                                            .argument("types", TFList.build(
+                                                    restApi.endpointConfiguration()
+                                                            .typesAsStrings().stream()
+                                                            .map(type -> TFString.builder().isLineIndent(false).value(type)
+                                                                    .build())
+                                                            .collect(Collectors.toList())
+                                            ))
+                                            .argument("vpc_endpoint_ids", TFList.build(
+                                                    restApi.endpointConfiguration()
+                                                            .vpcEndpointIds().stream()
+                                                            .map(type -> TFString.build(
+                                                                    MessageFormat.format("aws_vpc_endpoint.{0}.id", type)))
+                                                            .collect(Collectors.toList())
+                                            ))
+                                            .build()
+                                    )
+                                    .build())
+                    .build();
+
+            awsRestApi.getAwsStages().forEach(awsStage -> {
+                        Stage stage = awsStage.getStage();
+                        resourceMapsBuilder.map(
+                                Resource.builder()
+                                        .api("aws_api_gateway_stage")
+                                        .name(stage.stageName())
+                                        .argument("rest_api_id", TFExpression.build(
+                                                MessageFormat.format("aws_api_gateway_rest_api.{0}.id", restApi.name())))
+                                        .argument("deployment_id", TFExpression.build(
+                                                MessageFormat.format("aws_api_gateway_deployment.{0}.id", stage.deploymentId())))
+                                        .argument("stage_name", TFString.build(stage.stageName()))
+                                        .argument("xray_tracing_enabled", TFBool.build(stage.tracingEnabled()))
+                                        .argumentIf(Optional.ofNullable(stage.accessLogSettings()).isPresent(),
+                                                "access_log_settings",
+                                                () -> TFBlock.builder()
+                                                        .argument("destination_arn", TFString.build(stage.accessLogSettings().destinationArn()))
+                                                        .argument("format", TFString.build(stage.accessLogSettings().format()))
+                                                        .build())
+                                        .build());
+
+                        GetDeploymentResponse deployment = awsStage.getDeployment();
+
+                        resourceMapsBuilder.map(
+                                Resource.builder()
+                                        .api("aws_api_gateway_deployment")
+                                        .name(deployment.id())
+                                        .argument("rest_api_id", TFExpression.build(
+                                                MessageFormat.format("aws_api_gateway_rest_api.{0}.id", restApi.name())))
+                                        .argument("stage_name", TFString.build(stage.stageName()))
+                                        .argument("description", TFString.build(deployment.description()))
+                                        .build());
+                    }
+            );
+
+
+        }
+        return resourceMapsBuilder.build();
+    }
+}
---
+++ terraform-generator-aws/src/test/java/com/anthunt/terraform/generator/aws/service/apigateway/ExportApiGatewayTest.java
@@ -0,0 +1,86 @@
+package com.anthunt.terraform.generator.aws.service.apigateway;
+
+import com.anthunt.terraform.generator.aws.client.AmazonClients;
+import com.anthunt.terraform.generator.aws.service.apigateway.model.AWSRestApi;
+import com.anthunt.terraform.generator.aws.service.apigateway.model.AWSStage;
+import com.anthunt.terraform.generator.aws.support.DisabledOnNoAwsCredentials;
+import com.anthunt.terraform.generator.aws.support.TestDataFileUtils;
+import com.anthunt.terraform.generator.core.model.terraform.nodes.Maps;
+import com.anthunt.terraform.generator.core.model.terraform.nodes.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ResourceLoader;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.apigateway.ApiGatewayClient;
+import software.amazon.awssdk.services.apigateway.model.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+@SpringBootTest(classes = {AmazonClients.class})
+class ExportApiGatewayTest {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    private static ExportApiGatewayRestApis exportApigatewayRestApis;
+
+    private static ApiGatewayClient client;
+
+    @BeforeAll
+    public static void beforeAll() {
+        exportApigatewayRestApis = new ExportApiGatewayRestApis();
+        AmazonClients amazonClients = AmazonClients.builder().profileName("default").region(Region.AP_NORTHEAST_2).build();
+        client = amazonClients.getApiGatewayClient();
+    }
+
+    @Test
+    @DisabledOnNoAwsCredentials
+    public void export() {
+        Maps<Resource> export = exportApigatewayRestApis.export(client, null, null);
+        log.debug("export => \n{}", export.unmarshall());
+    }
+
+    @Test
+    public void getResourceMaps() {
+        List<AWSRestApi> awsRestApis = List.of(
+                AWSRestApi.builder()
+                        .restApi(RestApi.builder()
+                                .name("PetStore")
+                                .description("Your first API with Amazon API Gateway. This is a sample API that integrates via HTTP with our demo Pet Store endpoints")
+                                .apiKeySource(ApiKeySourceType.HEADER)
+                                .disableExecuteApiEndpoint(false)
+                                .endpointConfiguration(EndpointConfiguration.builder()
+                                        .types(EndpointType.REGIONAL)
+                                        .build())
+                                .build()
+                        )
+                        .awsStage(AWSStage.builder()
+                                .stage(Stage.builder()
+                                        .stageName("PROD")
+                                        .deploymentId("0t5yci")
+                                        .tracingEnabled(false)
+                                        .build())
+                                .deployment(GetDeploymentResponse.builder()
+                                        .id("0t5yci")
+                                        .description("test deploy")
+                                        .build())
+                                .build())
+                        .build());
+
+        Maps<Resource> resourceMaps = exportApigatewayRestApis.getResourceMaps(awsRestApis);
+        String actual = resourceMaps.unmarshall();
+
+        log.debug("actual => \n{}", actual);
+        String expected = TestDataFileUtils.asString(
+                resourceLoader.getResource("testData/apigateway/expected/Apigateway.tf")
+        );
+        assertEquals(expected, actual);
+    }
+
+}
