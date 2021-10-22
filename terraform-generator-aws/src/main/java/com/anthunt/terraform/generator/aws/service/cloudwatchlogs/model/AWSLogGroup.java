ef45c4571f56a8f6a6b18abdfe399eac5586dfae
최정열 <cinnamond3@lgcns.com>
2021-10-22 16:39:46 +0900

    refactoring/add-TerraformSource-to-AWS-models

--- terraform-generator-aws/src/main/java/com/anthunt/terraform/generator/aws/service/cloudwatchlogs/model/AWSLogGroup.java
+++ terraform-generator-aws/src/main/java/com/anthunt/terraform/generator/aws/service/cloudwatchlogs/model/AWSLogGroup.java
@@ -1,5 +1,6 @@
 package com.anthunt.terraform.generator.aws.service.cloudwatchlogs.model;
 
+import com.anthunt.terraform.generator.core.model.terraform.TerraformSource;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Singular;
@@ -11,9 +12,30 @@ import java.util.Map;
 @Data
 @ToString
 @Builder
-public class AWSLogGroup {
+public class AWSLogGroup implements TerraformSource {
+
+    private static final String TERRAFORM_RESOURCE_NAME = "aws_cloudwatch_log_group";
+
     private LogGroup logGroup;
 
     @Singular
-    private Map<String,String> tags;
+    private Map<String, String> tags;
+
+    @Override
+    public String getTerraformResourceName() {
+        return TERRAFORM_RESOURCE_NAME;
+    }
+
+    @Override
+    public String getResourceId() {
+        return logGroup.logGroupName();
+    }
+
+    @Override
+    public String getResourceName() {
+        String logGroupName = logGroup.logGroupName();
+        return logGroupName.startsWith("/") ?
+                logGroupName.substring(1).replaceAll("/", "-")
+                : logGroupName.replaceAll("/", "-");
+    }
 }
