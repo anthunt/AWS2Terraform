01e8759c88121da78fce3ee3b1cc55d04a3bc937
최정열 <cinnamond3@lgcns.com>
2021-10-25 15:58:47 +0900

    refactoring/add-TerraformSource-to-AWS-models

--- terraform-generator-aws/src/main/java/com/anthunt/terraform/generator/aws/service/elasticsearch/model/AWSElasticsearchDomain.java
+++ terraform-generator-aws/src/main/java/com/anthunt/terraform/generator/aws/service/elasticsearch/model/AWSElasticsearchDomain.java
@@ -1,5 +1,6 @@
 package com.anthunt.terraform.generator.aws.service.elasticsearch.model;
 
+import com.anthunt.terraform.generator.core.model.terraform.TerraformSource;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Singular;
@@ -12,9 +13,27 @@ import java.util.List;
 @Data
 @Builder
 @ToString
-public class AWSElasticsearchDomain {
+public class AWSElasticsearchDomain implements TerraformSource {
+
+    private static final String TERRAFORM_RESOURCE_NAME = "aws_elasticsearch_domain";
+
     private ElasticsearchDomainStatus elasticsearchDomainStatus;
 
     @Singular
     private List<Tag> tags;
+
+    @Override
+    public String getTerraformResourceName() {
+        return TERRAFORM_RESOURCE_NAME;
+    }
+
+    @Override
+    public String getResourceId() {
+        return elasticsearchDomainStatus.domainName();
+    }
+
+    @Override
+    public String getResourceName() {
+        return elasticsearchDomainStatus.domainName();
+    }
 }
